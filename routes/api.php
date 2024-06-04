4b19b2f61962eebeb4f8363733c1fd971730e2bb
Ahsan Mahmood <aoneahsan@gmail.com>
2024-06-04 15:58:13 +0500

    laravel sanctum package implemented

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Zaions\TestController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,10 +17,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::delete('/logout', function (Request $request) {
+        try {
+            $user = $request->user();
+            $user->tokens()->delete();
+            return response()->json(['success' => true]);
+        } catch (\Throwable $th) {
+            return response()->json(['success' => false]);
+        }
+    });
 });
 
 Route::controller(TestController::class)->group(function () {
     Route::get('/test-api-1', 'testApi1');
+});
+
+Route::get('/test-sanctum', function () {
+    $user = User::where('email', '=', 'aoneahsan@gmail.com')->first();
+    if (!$user) {
+        $user = User::create([
+            'email' => 'aoneahsan@gmail.com',
+            'password' => Hash::make('Asd123!@#')
+        ]);
+    }
+
+
+    $token = $user->createToken('auth-token');
+
+    return response()->json(['user' => $user, 'token' => $token]);
 });
